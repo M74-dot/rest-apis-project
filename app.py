a36e9337700e5aa87f66052d05b45d3fb2031b28
M74-dot <githubmanisha123@gmail.com>
2023-04-21 15:05:51 +0530

    feat(docker): run our flask app using gunicorn

--- app.py
+++ app.py
@@ -30,9 +30,7 @@ def create_app(db_url=None):
         = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-
     migrate = Migrate(app, db)
-
     api = Api(app)
 
     app.config["JWT_SECRET_KEY"] = "jose"
